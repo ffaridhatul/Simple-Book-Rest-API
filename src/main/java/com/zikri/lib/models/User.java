5e902907ef8716aae73a0dcfefc14f395ac5e7a5
ffaridhatul <faridhatulzikri11@gmail.com>
2025-01-19 22:12:21 +0700

    Add User

---
+++ src/main/java/com/zikri/lib/models/User.java
@@ -0,0 +1,28 @@
+package com.zikri.lib.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+
+
+}
